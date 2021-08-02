e2c006574ec408885d6ee468f1987baaebd89539
AhmedMoamen360 <87285086+AhmedMoamen360@users.noreply.github.com>
2021-08-02 15:23:52 +0200

    solve linking problem

--- include/to-do-list/sortedmodel.h
+++ include/to-do-list/sortedmodel.h
@@ -5,8 +5,7 @@
 
 struct Data {
     Data() {}
-    Data(const QString& name, const QString& type, const QString& task) : name(name), type(type), task(task) {}
-    QString name;
+    Data(const QString& type, const QString& task) : type(type), task(task) {}
     QString type;
     QString task;
 };
@@ -14,16 +13,16 @@ struct Data {
 class SortedModel : public QAbstractListModel
 {
     Q_OBJECT
+
 public:
     enum Roles{
-        NameRole = Qt::UserRole,
         TypeRole,
         TaskRole
     };
 
     explicit SortedModel(QObject *parent = nullptr);
     int rowCount(const QModelIndex& parent) const override;
-    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+    QVariant data( const QModelIndex& index, int role = Qt::DisplayRole ) const override;
     QHash<int, QByteArray> roleNames() const override;
 
 public slots:
--- src/to-do-list/sortedmodel.cpp
+++ src/to-do-list/sortedmodel.cpp
@@ -1,9 +1,14 @@
 #include "include/to-do-list/sortedmodel.h"
 #include <QByteArray>
 
-SortedModel::SortedModel(QObject *parent)
+SortedModel::SortedModel(QObject *parent) : QAbstractListModel(parent)
 {
-
+    m_data
+        << Data("To Do", "Test")
+        << Data("Completed", "Search")
+        << Data("Completed", "Design")
+        << Data("To Do", "Deploy")
+        << Data("Completed", "Develop");
 }
 
 int SortedModel::rowCount(const QModelIndex &parent) const
@@ -20,9 +25,7 @@ QVariant SortedModel::data(const QModelIndex &index, int role) const
         return QVariant();
 
     const Data& data = m_data.at(index.row());
-    if(role == NameRole)
-        return data.name;
-    else if(role == TypeRole)
+    if(role == TypeRole)
         return data.type;
     else if(role == TaskRole)
         return data.task;
@@ -33,9 +36,18 @@ QVariant SortedModel::data(const QModelIndex &index, int role) const
 QHash<int, QByteArray> SortedModel::roleNames() const
 {
     static QHash<int, QByteArray> mapping {
-        {NameRole, "name"},
         {TypeRole, "type"},
         {TaskRole, "task"}
     };
     return mapping;
+}
+
+void SortedModel::duplicateData(int row)
+{
+
+}
+
+void SortedModel::removeData(int row)
+{
+
 }
